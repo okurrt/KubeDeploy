55ef4e5fbb9e67c2b0ad8c61cb394efb2a1da355
aditiu <aditi.18.coool@gmail.com>
2018-10-03 14:43:03 +0530

    added files

---
+++ ansible/mysqlImage/Dockerfile
@@ -0,0 +1,4 @@
+FROM mysql:5.7.22
+MAINTAINER Aditi Upadhyay <aditi.18.coool@gmail.com>
+
+COPY ./demo.sql /docker-entrypoint-initdb.d/
---
+++ ansible/mysqlImage/demo.sql
@@ -0,0 +1,8 @@
+CREATE TABLE book (book_id int(11) NOT NULL,title varchar(128) NOT NULL,
+  author varchar(45) NOT NULL,
+  price float NOT NULL,
+  PRIMARY KEY (book_id)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+
+
+insert into book values (10,'harry','aditi',120.0);
